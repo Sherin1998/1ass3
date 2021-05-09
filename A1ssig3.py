5088d1a4fba3dcf6d42c46879cfa5c3aab8a99d6
Sherin1998 <75293746+Sherin1998@users.noreply.github.com>
2021-05-09 11:41:48 +0530

    Add files via upload

---
+++ A1ssig3.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. It reduces the repetetion of code.If a program requires same tasks to be performed for different elements,functions can be       used instead of repeating the code again.
+# 
+
+# 2. Fuctions work when it is called in a program.Fuction call statement invokes the function in a program.It can be called by       function_name(parameters)
+# 
+
+# 3.  'def' keyword is used.It is followed by function_name()
+
+# 
+# 4.  Functions consists of 'def' statement and are used to store the set of procedures,type of arguments,return types as entered by user.
+# Function call is used  to invoke the function statement by creating a object and  specifying parameters along with return type.
+
+# In[40]:
+
+
+5. 1 global scope.It can be accessed anywhere within program
+   Local scopes are within class/function in which it is defined.They exist whenever function is called
+
+
+# 6. The variables stored initially gets changed and new value returned by function is stord.The scope is removed.
+
+# 7.
+# Return statement is used in function to return value to fuction call statement as specified by user.It can be of expression type. 
+# 
+
+# In[39]:
+
+
+8. if no return statement is available in function ,it is none 
+
+
+# 9. Global statement will map a variable in a function to global variable
+
+# ###### 
+
+# In[47]:
+
+
+10.  None type
+
+
+# In[ ]:
+
+
+11. imports the module 'areallyourpetsnamederic'
+
+
+# In[ ]:
+
+
+12. spam.bacon()
+
+
+# 13. 
+
+# In[ ]:
+
+
+14. The line of code having chance to cause error moves tp try clause 
+    The code code that executes if error occurs moves to except clause
+
